1843b5a49be77e32fd55a03a114ddffda8f4c972
jmelesky <code@phaedrusdeinus.org>
2016-03-10 22:04:19 -0800

    add argument parsing

--- intertimer.py
+++ intertimer.py
@@ -16,4 +16,29 @@ def main(intervals, loopcount):
 if __name__ == '__main__':
     import argparse
 
-    main([{'length': 5, 'message': 'interval 1'}, {'length': 2, 'message': 'interval 2'}], 3)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-i', '--interval', action='append')
+    parser.add_argument('-n', '--name', action='append')
+    parser.add_argument('-c', '--loops', default='5', type=int)
+
+    args = parser.parse_args()
+
+    lengths = []
+    if args.interval:
+        lengths = args.interval
+
+    names = []
+    if args.name:
+        names = args.name
+
+    intervals = []
+    for i in range(0, len(lengths)):
+        name = 'Interval %s' % (i + 1)
+        if len(names) > i:
+            name = names[i]
+        intervals += [{'length': int(lengths[i]),
+                       'message': name}]
+
+    main(intervals, args.loops)
+
